95f8d9d1c23d028241e318ee9309e3dfe8e39925
haochangfei <34223852+haochangfei@users.noreply.github.com>
2018-03-11 12:05:04 +0800

    Create isPrime.cpp

---
+++ isPrime.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define maxn 2000005
+using namespace std;
+
+long long num[maxn];//save Prime
+bool isPrime[maxn];//mark
+
+int main()
+{
+	int n;
+	cin>>n;
+	int cnt = 1;//count Prime
+	long long sum = 0;
+	memset(isPrime,1,sizeof(isPrime));//all isPrime
+	isPrime[0] = isPrime[1] = 0; // o and 1 is not
+	for(int i=2;i<=n;i++)
+	{
+		if(isPrime[i]) 
+		{
+			num[cnt++] = i; 
+			sum+=i;
+		}
+		for(int j=1;j<cnt&&num[j]*i<=n;j++)
+		{
+			isPrime[num[j]*i] = 0;//prime <= i :prime * i  
+		}
+	}
+	cout<<sum<<endl;
+	return 0;
+}
